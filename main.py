2f824027dbb62cdb65a6b66026203d2502faa923
Kevin <71783685+kbrault@users.noreply.github.com>
2021-12-02 22:48:37 +0100

    Correction of search_by_meta_generator + addition of search_by_included_version + exceptions catching

--- main.py
+++ main.py
@@ -2,25 +2,52 @@ import requests
 import re
 
 url = "https://blender.org"
-r = requests.get(url)
 
-
-def search_by_meta_generator():
+def search_by_meta_generator(request):
+    """
+    search_by_meta_generator : If exist, return the XXX wordpress version value of <meta name="generator" content="Wordpress XXX"/> 
+    :return: If exist, return the Wordpress version of the meta generator
+    """
     # How about some beautifulsoup to avoid these naughty regex ?
     regex = r"<meta.*?name=\"generator\".*?content=\"WordPress (.*?)\".*?>"
-
-    matches = re.findall(regex, r.content.decode("utf-8"), re.MULTILINE)
-    if not matches:
-        pass
+    matches = re.findall(regex, request.content.decode("utf-8"), re.MULTILINE)
+    if matches:
+        return matches[0] 
     else:
-       return matches[0]
+        return "Undefined"
 
+
+def search_by_included_version(request):
+    """
+    search_by_included_version : If exist, return the Wordpress version of the included CSS file of the /wp-admin/install.php URI
+    :return: If exist, return the Wordpress version of the included CSS file
+    """
+    response_install_file = requests.get(url+'/wp-admin/install.php')
+    if response_install_file.status_code == 200:
+        regex = r"wp-admin\/css\/install\.min\.css\?ver\=(.*?)'"
+        matches = re.findall(regex, response_install_file.content.decode("utf-8"), re.MULTILINE)
+        if matches:
+            return matches[0]
+        else:
+            return "Undefined"
+    else:
+            return "Undefined"
+        
 # TODO :
-# def search_by_readme():
 # def search_by_feed_meta_generator():
-# def search_by_included_version():
+# def search_by_readme_file():
 # def search_by_md5_files():
 
-
 if __name__ == "__main__":
-    print(search_by_meta_generator())
+    try: 
+        r = requests.get(url,timeout=3)
+        print(f'Meta Generator Version : '+search_by_meta_generator(r))
+        print(f'Included CSS Version : '+search_by_included_version(r))
+    except requests.exceptions.HTTPError as error_http:
+        print ("HTTP Error :",error_http)
+    except requests.exceptions.ConnectionError as error_connexion:
+        print ("Connexion Error :",error_connexion)
+    except requests.exceptions.Timeout as error_timeout:
+        print ("Timeout Error :",error_timeout)
+    except requests.exceptions.RequestException as error:
+        print ("OOps: Something Else",error)
